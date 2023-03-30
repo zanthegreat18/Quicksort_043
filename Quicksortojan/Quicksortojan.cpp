c2edb8b2b6309b5e2e47c83470d018cf3bbd734b
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-03-30 10:52:20 +0700

    quicksort2

--- Quicksortojan/Quicksortojan.cpp
+++ Quicksortojan/Quicksortojan.cpp
@@ -17,4 +17,21 @@ void input() {
 		else
 			cout << "nMaksimum panjang array adalah 20" << endl;
 	}
+
+	cout << "\n=====================" << endl;
+	cout << "\nEnter Array Element" << endl;
+	cout << "\n=====================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+} 
+
+void swap(int x, int y)
+{
+	int temp;
+
+	temp = arr[x];
 }

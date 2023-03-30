5321d9784eb45ea6142bdc2ff253585d007b62b3
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-03-30 10:32:52 +0700

    Add project files.

---
+++ Quicksortojan/Quicksortojan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array: ";
+	}
+}
